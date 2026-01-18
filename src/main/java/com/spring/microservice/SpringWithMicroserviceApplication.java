c3cadf5cfa5d06a05d89d2c118bf423b9d08127c
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-01-18 11:34:11 +0530

    upload file

---
+++ src/main/java/com/spring/microservice/SpringWithMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithMicroserviceApplication.class, args);
+	}
+
+}
